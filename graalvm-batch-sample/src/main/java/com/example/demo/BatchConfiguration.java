14c12825cd89321aa2209615eb56474dd104fc6f
Tomoya Sasaki <t.sasaki@future-shop.co.jp>
2025-01-27 10:09:43 +0900

    first commit

---
+++ graalvm-batch-sample/src/main/java/com/example/demo/BatchConfiguration.java
@@ -0,0 +1,53 @@
+package com.example.demo;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+@Configuration
+public class BatchConfiguration {
+
+	@Bean
+	public FlatFileItemReader<Member> itemReader() {
+		return new FlatFileItemReaderBuilder<Member>().name("personItemReader")
+				.resource(new ClassPathResource("member.csv")).linesToSkip(1).delimited()
+				.names("id", "firstName", "lastName").targetType(Member.class).build();
+	}
+
+	@Bean
+	public MemberItemProcessor processor() {
+		return new MemberItemProcessor();
+	}
+
+	@Bean
+	public ItemWriter<FullNameMember> writer() {
+		return chunk -> {
+			for (FullNameMember fullNameMember : chunk) {
+				System.out.println(fullNameMember.id() + ":" + fullNameMember.firstName() + ":"
+						+ fullNameMember.lastName() + ":" + fullNameMember.fullName());
+			}
+		};
+	}
+
+	@Bean
+	public Step step(JobRepository jobRepository, DataSourceTransactionManager transactionManager,
+			FlatFileItemReader<Member> reader, MemberItemProcessor processor, ItemWriter<FullNameMember> writer) {
+		return new StepBuilder("step", jobRepository).<Member, FullNameMember>chunk(3, transactionManager)
+				.reader(reader).processor(processor).writer(writer).build();
+	}
+
+	@Bean
+	public Job printDataJob(JobRepository jobRepository, Step step) {
+		return new JobBuilder("printDataJob-" + System.currentTimeMillis(), jobRepository).start(step).build();
+	}
+
+}
---
+++ graalvm-batch-sample/src/main/java/com/example/demo/GraalvmBatchSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraalvmBatchSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraalvmBatchSampleApplication.class, args);
+	}
+
+}
